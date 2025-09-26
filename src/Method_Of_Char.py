8fdbe0ac54e3ed95fb6c99914110612c0b823d3f
am975eng <15200369+am975eng@users.noreply.github.com>
2025-09-25 20:16:36 -0400

    Refactored minimum length nozzle iterator to remove redundancy. Added support for indexing full length nozzle code

--- src/Method_Of_Char.py
+++ src/Method_Of_Char.py
@@ -44,154 +44,127 @@ def gen_MOC_MLN(M_exit, r_throat, k=1.4, div=7, print_flag=False, plot_flag=Fals
 
     theta_n[0] = theta_wall_max 
     delta_theta = (theta_wall_max - init_theta)/(div-1)
-    start = 1
     y_n[0] = r_throat
     x_n[0] = 0
     x_contour.append(x_n[0])
     y_contour.append(y_n[0])
 
-    for size in range(div, 0, -1):  # The number of intersecting interior points decreases by 1 with each iteration
-        end = start + size - 1
-        if start == 1:              # Prandtl-Meyer expansion fan
-            for i in range(start,end+1):
-                theta_n[i] = init_theta + delta_theta * (i-1) 
-                nu_n[i] = theta_n[i]
-                K_minus[i] = theta_n[i] + nu_n[i]       # Char line right running strength constant
-                K_plus[i] = theta_n[i] - nu_n[i]        # Char line left running
-                M_n[i] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[i], k)).root # Local Mach number
-                mu_n[i] = np.arcsin(1/M_n[i])
-                if i == 1:
-                    m = np.tan(theta_n[i] - mu_n[i])
-                    x_n[i] = -y_n[0] / m
-                    y_n[i] = 0
+    
+    idx = 1                                         # Current point index                     
+    row = 1                                         # Vertical row index
+    while idx <= npts-1:
+        row_len = div - row + 2
+        for row_index in range(row_len):            # Iterate through each row
+            if row_index==0:                        # Centerline Point
+                if idx == 1:                        # First PM centerline point
+                    theta_n[idx] = init_theta + delta_theta * (idx-1) 
+                    nu_n[idx] = theta_n[idx]
+                    K_minus[idx] = theta_n[idx] + nu_n[idx]       # Char line right running strength constant
+                    K_plus[idx] = theta_n[idx] - nu_n[idx]        # Char line left running
+                    M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root # Local Mach number
+                    mu_n[idx] = np.arcsin(1/M_n[idx])
+                    m = np.tan(theta_n[idx] - mu_n[idx])
+                    x_n[idx] = -y_n[0] / m
+                    y_n[idx] = 0
                     if plot_flag:
-                        plt.plot([x_n[0], x_n[i]], [y_n[0], y_n[i]], 'k-', linewidth=2)
-                        plt.annotate(str(i), (x_n[i],y_n[i]))
-                else:
-                    m_1=np.tan(theta_n[i] - mu_n[i])
+                        plt.plot([x_n[0], x_n[idx]], [y_n[0], y_n[idx]], 'k-', linewidth=2)
+                        plt.annotate(str(idx), (x_n[idx],y_n[idx]))
+                else:                               # Other centerline points
+                    K_minus[idx] = K_minus[idx-row_len]
+                    theta_n[idx] = 0
+                    K_plus[idx] = -K_minus[idx]
+                    nu_n[idx] = 0.5*(K_minus[idx] - K_plus[idx])
+                    M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root
+                    mu_n[idx] = np.arcsin(1/M_n[idx])
+                    
+                    m_1 = np.tan(theta_n[idx-row_len] - mu_n[idx-row_len])            
+                    x_1 = x_n[idx-row_len]
+                    y_1 = y_n[idx-row_len]
+                    x_n[idx] = x_1 -y_1 / m_1
+                    y_n[idx] = 0
+                    
+                    if plot_flag:
+                        plt.plot([x_1,x_n[idx]], [y_1,y_n[idx]], 'k-', linewidth=2)
+                        plt.annotate(str(idx), (x_n[idx],y_n[idx]))
+            elif row_index == row_len-1:            # Wall Point
+                theta_n[idx] = theta_n[idx-1]
+                nu_n[idx] = nu_n[idx-1]
+                K_minus[idx] = K_minus[idx-1]
+                K_plus[idx] = K_plus[idx-1]
+                M_n[idx] = M_n[idx-1]
+                mu_n[idx] = mu_n[idx-1]
+
+                theta_start = 0.5*(theta_n[idx-row_len] + theta_n[idx])
+                m_1 = np.tan(theta_start)
+                x_1 = x_n[idx-row_len]
+                y_1 = y_n[idx-row_len]
+                m_2 = np.tan(theta_n[idx] + mu_n[idx])
+                x_2 = x_n[idx-1]
+                y_2 = y_n[idx-1]
+                x_3 = (m_1*x_1 - m_2*x_2 + y_2 - y_1)/(m_1-m_2)
+                y_3 = y_1 + m_1*(x_3 - x_1)
+                x_n[idx]=x_3
+                y_n[idx]=y_3
+                x_contour.append(x_3)
+                y_contour.append(y_3)
+
+                if plot_flag:
+                    plt.plot([x_1, x_3], [y_1, y_3], 'r-', linewidth=2)
+                    plt.plot([x_2, x_3], [y_2, y_3], 'k-', linewidth=2)
+                    plt.annotate(str(idx), (x_3,y_3))
+            else:                                   # Interior grid intersection point
+                if row == 1:                        # PM fan interior grid points
+                    theta_n[idx] = init_theta + delta_theta * (idx-1) 
+                    nu_n[idx] = theta_n[idx]
+                    K_minus[idx] = theta_n[idx] + nu_n[idx]       # Char line right running strength constant
+                    K_plus[idx] = theta_n[idx] - nu_n[idx]        # Char line left running
+                    M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root # Local Mach number
+                    mu_n[idx] = np.arcsin(1/M_n[idx])
+                
+                    m_1=np.tan(theta_n[idx] - mu_n[idx])
                     x_1=x_n[0]
                     y_1=y_n[0]
-                    m_2=np.tan(theta_n[i-1] + mu_n[i-1])
-                    x_2=x_n[i-1]
-                    y_2=y_n[i-1]
+                    m_2=np.tan(theta_n[idx-1] + mu_n[idx-1])
+                    x_2=x_n[idx-1]
+                    y_2=y_n[idx-1]
                     x_3 = (m_1*x_1 - m_2*x_2 + y_2 - y_1)/(m_1-m_2)
                     y_3 = y_1 + m_1*(x_3 - x_1)
-                    x_n[i] = x_3
-                    y_n[i] = y_3
+                    x_n[idx] = x_3
+                    y_n[idx] = y_3
                     if plot_flag:
-                        plt.plot([x_n[0], x_n[i]], [y_n[0], y_n[i]], 'k-', linewidth=2)
-                        plt.plot([x_n[i-1], x_n[i]], [y_n[i-1], y_n[i]], 'k-', linewidth=2)
-                        plt.annotate(str(i), (x_n[i],y_n[i]))
+                        plt.plot([x_n[0], x_n[idx]], [y_n[0], y_n[idx]], 'k-', linewidth=2)
+                        plt.plot([x_n[idx-1], x_n[idx]], [y_n[idx-1], y_n[idx]], 'k-', linewidth=2)
+                        plt.annotate(str(idx), (x_n[idx],y_n[idx]))
                 
-            theta_n[i+1] = theta_n[i]
-            nu_n[i+1] = nu_n[i]
-            K_minus[i+1] = K_minus[i]
-            K_plus[i+1] = K_plus[i]
-            M_n[i+1] = M_n[i]
-            mu_n[i+1] = mu_n[i]
-
-            theta_start = 0.5*(theta_n[0] + theta_n[i+1])
-            m_1 = np.tan(theta_start)
-            x_1 = x_n[0]
-            y_1 = y_n[0]
-            m_2 = np.tan(theta_n[i+1] + mu_n[i+1])
-            x_2 = x_n[i]
-            y_2 = y_n[i]
-            x_3 = (m_1*x_1 - m_2*x_2 + y_2 - y_1)/(m_1-m_2)
-            y_3 = y_1 + m_1*(x_3 - x_1)
-            x_n[i+1]=x_3
-            y_n[i+1]=y_3
-            x_contour.append(x_3)
-            y_contour.append(y_3)
-            if plot_flag:
-                plt.plot([x_1, x_3], [y_1, y_3], 'r-', linewidth=2)
-                plt.plot([x_2, x_3], [y_2, y_3], 'k-', linewidth=2)
-                plt.annotate(str(i+1), (x_3,y_3))
-
-        else:
-            for i in range(start,end+1):
-                if i == start:
-                    # Centerline grid point
-                    K_minus[i] = K_minus[i-size-1]
-                    theta_n[i] = 0
-                    K_plus[i] = -K_minus[i]
-                    nu_n[i] = 0.5*(K_minus[i] - K_plus[i])
-                    M_n[i] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[i], k)).root
-                    mu_n[i] = np.arcsin(1/M_n[i])
+                else:                               # Interior grid points
+                    K_minus[idx] = K_minus[idx-row_len]
+                    K_plus[idx] = K_plus[idx-1]
+                    theta_n[idx] = 0.5*(K_minus[idx] + K_plus[idx])
+                    nu_n[idx] = 0.5*(K_minus[idx] - K_plus[idx])
+                    M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root
+                    mu_n[idx] = np.arcsin(1/M_n[idx])
                     
-                    m_1 = np.tan(theta_n[i-size-1] - mu_n[i-size-1])            
-                    x_1 = x_n[i-size-1]
-                    y_1 = y_n[i-size-1]
-                    x_n[i] = x_1 -y_1 / m_1
-                    y_n[i] = 0
-                    
-                    if plot_flag:
-                        plt.plot([x_1,x_n[i]], [y_1,y_n[i]], 'k-', linewidth=2)
-                        plt.annotate(str(i), (x_n[i],y_n[i]))
-                else:
-                    # Interior intersection grid point
-                    K_minus[i] = K_minus[i-size-1]
-                    K_plus[i] = K_plus[i-1]
-                    theta_n[i] = 0.5*(K_minus[i] + K_plus[i])
-                    nu_n[i] = 0.5*(K_minus[i] - K_plus[i])
-                    M_n[i] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[i], k)).root
-                    mu_n[i] = np.arcsin(1/M_n[i])
-                    
-                    m_1 = np.tan(theta_n[i-1] + mu_n[i-1])
-                    x_1 = x_n[i-1]
-                    y_1 = y_n[i-1]
-                    m_2 = np.tan(theta_n[i-size-1]  - mu_n[i-size-1] )
-                    x_2 = x_n[i-size-1]
-                    y_2 = y_n[i-size-1]
+                    m_1 = np.tan(theta_n[idx-1] + mu_n[idx-1])
+                    x_1 = x_n[idx-1]
+                    y_1 = y_n[idx-1]
+                    m_2 = np.tan(theta_n[idx-row_len]  - mu_n[idx-row_len] )
+                    x_2 = x_n[idx-row_len]
+                    y_2 = y_n[idx-row_len]
                     x_3 = (m_1*x_1 - m_2*x_2 + y_2 - y_1)/(m_1-m_2)
                     y_3 = y_1 + m_1*(x_3 - x_1)
-                    x_n[i] = x_3
-                    y_n[i] = y_3
+                    x_n[idx] = x_3
+                    y_n[idx] = y_3
                     
                     if plot_flag:
-                        plt.plot([x_n[i-1], x_n[i]], [y_n[i-1], y_n[i]], 'k-', linewidth=2)
-                        plt.plot([x_n[i-size-1], x_n[i]], [y_n[i-size-1], y_n[i]], 'k-', linewidth=2)
-                        plt.annotate(str(i), (x_n[i],y_n[i]))
-
-            # Wall Contour
-            theta_n[i+1] = theta_n[i]
-            nu_n[i+1] = nu_n[i]
-            K_minus[i+1] = K_minus[i]
-            K_plus[i+1] = K_plus[i]
-            M_n[i+1] = M_n[i]
-            mu_n[i+1] = mu_n[i]
-
-            theta_start = 0.5*(theta_n[i-size] + theta_n[i+1])
-            m_1 = np.tan(theta_start)
-            x_1 = x_n[i-size]
-            y_1 = y_n[i-size]
-            m_2 = np.tan(theta_n[i] + mu_n[i])
-            x_2 = x_n[i]
-            y_2 = y_n[i]
-            x_3 = (m_1*x_1 - m_2*x_2 + y_2 - y_1)/(m_1-m_2)
-            y_3 = y_1 + m_1*(x_3 - x_1)
-            x_n[i+1]=x_3
-            y_n[i+1]=y_3
-            x_contour.append(x_3)
-            y_contour.append(y_3)
-
-            if plot_flag:
-                plt.plot([x_1, x_3], [y_1, y_3], 'r-', linewidth=2)
-                plt.plot([x_2, x_3], [y_2, y_3], 'k-', linewidth=2)
-                plt.annotate(str(i+1), (x_3,y_3))                    
-
-        K_minus[end+1] = K_minus[end]
-        K_plus[end+1] = K_plus[end]
-        theta_n[end+1] = theta_n[end]
-        nu_n[end+1] = nu_n[end]
-        M_n[end+1] = M_n[end]
-        mu_n[end+1] = mu_n[end]
-        
-        start = end + 2  # move past block + skip 1
+                        plt.plot([x_n[idx-1], x_n[idx]], [y_n[idx-1], y_n[idx]], 'k-', linewidth=2)
+                        plt.plot([x_n[idx-row_len], x_n[idx]], [y_n[idx-row_len], y_n[idx]], 'k-', linewidth=2)
+                        plt.annotate(str(idx), (x_n[idx],y_n[idx]))                      
+            idx +=1
+        row += 1
 
     if print_flag == True:
         for i in range(len(theta_n)):
-            print(f"i {str(i)} K- {K_minus[i]*180/np.pi:.3f} K+ {K_plus[i]*180/np.pi:.3f} theta {theta_n[i]*180/np.pi:.3f} nu {nu_n[i]*180/np.pi:.3f} M {M_n[i]:.3f} mu {mu_n[i]*180/np.pi:.3f}")
+            print(f"i {str(i)} K- {K_minus[i]*180/np.pi:.3f} K+ {K_plus[i]*180/np.pi:.3f} theta {theta_n[i]*180/np.pi:.3f} nu {nu_n[i]*180/np.pi:.3f} M {M_n[i]:.3f} mu {mu_n[i]*180/np.pi:.3f} x {x_n[i]:.3f} y {y_n[i]:.3f}")
 
     return x_contour, y_contour, x_n, y_n, M_n
 
@@ -205,19 +178,15 @@ def gen_MOC_FLN(M_exit, r_throat, k=1.4, div=7, print_flag=False, plot_flag=Fals
     theta_wall_max = AT.calc_prandtl_meyer(M_exit, k)/2     # Maximum wall angle
     theta_final_expand = -(np.pi/2-theta_wall_max)          # Final angle of arc
     theta_expand = np.linspace(-np.pi/2, theta_final_expand, res)   # Theta for circular arc
-    scale_factor = 200
+    scale_factor = 5
     r_expand = r_throat * scale_factor                      # Radius of arc
-    y_center = r_throat * (scale_factor+1)                  # Center of arc
-    x_expand = r_expand * np.cos(theta_expand) 
-    y_expand = y_center + r_expand * np.sin(theta_expand)
 
     theta_expand = np.linspace(0,theta_wall_max, res)
     x_expand = r_expand * np.sin(theta_expand)
     y_expand = r_throat + r_expand * (1-np.cos(theta_expand))
 
     start = 1
-    npts = int(div * (div+1)/2 + div)                       # Total number of intersection points
-    npts += 1
+    npts = int(div*(div+1)/2 + 2*div)
     theta_n = np.zeros(npts)
     theta_n[0:div+1] = np.linspace(0,theta_wall_max, div+1)
     nu_n = np.zeros(npts)
@@ -227,30 +196,80 @@ def gen_MOC_FLN(M_exit, r_throat, k=1.4, div=7, print_flag=False, plot_flag=Fals
     K_plus = np.zeros(npts)
     x_n = np.zeros(npts)
     y_n = np.zeros(npts)
+    x_n[0] = 0
+    y_n[0] = r_throat
 
-    for size in range(div, 0, -1):  # The number of intersecting interior points decreases by 1 with each iteration
-        end = start + size - 1
-        if start == 1:              # Prandtl-Meyer expansion fan
-            for i in range(start,end+1):
-                nu_n[i] = theta_n[i]
-                K_minus[i] = theta_n[i] + nu_n[i]       # Char line right running strength constant
-                K_plus[i] = theta_n[i] - nu_n[i]        # Char line left running
-                M_n[i] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[i], k)).root 
-                mu_n[i] = AT.calc_prandtl_meyer(M_n[i], k)
+    idx = 1                                         # Current point index                     
+    row_len = div
+
+    while idx <= npts:
+        for row_index in range(row_len):            # Iterate through each row
+            if idx <= div:                          # Expansion section wall points
+                nu_n[idx] = theta_n[idx]
+                K_minus[idx] = theta_n[idx] + nu_n[idx]       # Char line right running strength constant
+                K_plus[idx] = theta_n[idx] - nu_n[idx]        # Char line left running
+                M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root 
+                mu_n[idx] = np.arcsin(1/M_n[idx])
                 
-                x_n[i] = r_expand * np.sin(theta_n[i])
-                y_n[i] = r_throat + r_expand * (1-np.cos(theta_n[i])) 
+                x_n[idx] = r_expand * np.sin(theta_n[idx])
+                y_n[idx] = r_throat + r_expand * (1-np.cos(theta_n[idx]))
+                row_len = div + 2
                 if plot_flag:
-                    plt.scatter(x_n[i], y_n[i], s=1)
-                    plt.annotate(str(i), (x_n[i],y_n[i]))
-                    
-    
+                    plt.scatter(x_n[idx], y_n[idx], s=1)
+                    plt.annotate(str(idx), (x_n[idx],y_n[idx]))
+
+            elif row_index == 0:                    # Centerline intercept
+                print(f"Center Line idx {idx}")
+                K_minus[idx] = K_minus[idx-div]
+                K_plus[idx] = -K_minus[idx]
+                theta_n[idx] = 0
+                nu_n[idx] = K_minus[idx]
+                M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root 
+                mu_n[idx] = np.arcsin(1/M_n[idx])
+
+                slope = (theta_n[idx-div] - mu_n[idx-div] - mu_n[idx])/2
+                x_n[idx] = - (y_n[idx-div] / np.tan(slope)) + x_n[idx-div]
+                y_n[idx] = 0
+
+                if plot_flag:
+                    plt.scatter(x_n[idx], y_n[idx], s=1)
+                    plt.annotate(str(idx), (x_n[idx],y_n[idx]))
+            elif row_index == row_len-1:      # Wall points
+                print(f"Wall idx {idx}")
+            else:                               # Interior grid points
+                #print(f"idx {idx}")
+                #print(f"idx - row index {idx - row_len}")
+                K_minus[idx] = K_minus[idx-row_len]
+                K_plus[idx] = K_plus[idx-1]
+                theta_n[idx] = (K_minus[idx] + K_plus[idx])/2
+                nu_n[idx] = (K_minus[idx] - K_plus[idx])/2
+                M_n[idx] = root_scalar(AT.RS_mach_prandtl_meyer, bracket=[1,10], args=(nu_n[idx], k)).root
+                mu_n[idx] = np.arcsin(1/M_n[idx])
+
+                m_1 = np.tan(theta_n[idx-1]+mu_n[idx-1])
+                x_1 = x_n[idx-1]
+                y_1 = y_n[idx-1]
+                m_2 = np.tan(theta_n[idx-row_len] - mu_n[idx-row_len])
+                x_2 = x_n[idx-row_len]
+                y_2 = y_n[idx-row_len]
+                x_3 = (m_1*x_1 - m_2*x_2 + y_2 - y_1)/(m_1-m_2)
+                y_3 = y_1 + m_1*(x_3 - x_1)
+                x_n[idx] = x_3
+                y_n[idx] = y_3
+
+                if plot_flag:
+                    plt.scatter(x_n[idx], y_n[idx], s=1)
+                    plt.annotate(str(idx), (x_n[idx],y_n[idx]))
+
+            idx += 1
+        row_len -= 1
+
     for i in range(len(theta_n)):
         print(f" i {i} theta_n {theta_n[i]*180/np.pi:.3f} nu_n {nu_n[i]*180/np.pi:.3f} K- {K_minus[i]*180/np.pi:.3f} K+ {K_plus[i]*180/np.pi:.3f} M_n {M_n[i]:.3f} mu_n {mu_n[i]*180/np.pi:.3f} x_n {x_n[i]:.3f} y_n {y_n[i]:.3f}")
-    plt.show()
+    
     # plt.plot(x_expand, y_expand, 'k-')
-    # plt.axis('equal')
-    # plt.show()
+    plt.axis('equal')
+    plt.show()
 
 if __name__ == "__main__":
     # x_contour, y_contour, x_n, y_n, M_n = gen_MOC_MLN(2.4,1,1.4,7, True, True)
@@ -277,4 +296,4 @@ if __name__ == "__main__":
     # plt.axis('equal')
     # plt.show()
 
-    x_contour, y_contour, x_n, y_n, M_n = gen_MOC_FLN(2.4,1,1.4,7, True, True)
+    gen_MOC_FLN(2.4,1,1.4,7, True, True)
